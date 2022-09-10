dc9eb10ba59b01e323f9164f92c086768065faef
mizbauddintareqe <mizbauddintareq@gmail.com>
2022-09-10 23:16:07 +0600

    solved problem two

--- practice-problems.js
+++ practice-problems.js
@@ -1,12 +1,26 @@
 // Problem No:- 1
-var fruits = ["Apple", "Banana", "Orange"];
+// var fruits = ["Apple", "Banana", "Orange"];
 
-var findBanana = fruits.indexOf("Banana");
-console.log(findBanana);
+// var findBanana = fruits.indexOf("Banana");
+// console.log(findBanana);
 
-fruits[1] = "Mango";
-console.log(fruits);
+// fruits[1] = "Mango";
+// console.log(fruits);
 
-fruits.pop();
-fruits.push("Watermelon");
-console.log(fruits);
+// fruits.pop();
+// fruits.push("Watermelon");
+// console.log(fruits);
+
+// Problem No:- 2
+var marks = 85;
+if (marks >= 80) {
+  console.log("congratulation's you have got A");
+} else if (marks >= 60) {
+  console.log("congratulation's you have got B");
+} else if (marks >= 50) {
+  console.log("congratulation's you have got C");
+} else if (marks >= 40) {
+  console.log("congratulation's you have got D");
+} else if (marks <= 39) {
+  console.log("Result not found.");
+}
