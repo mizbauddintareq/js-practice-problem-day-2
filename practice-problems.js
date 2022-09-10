de7ea3ec5539488aef9216d945804efea5106b33
mizbauddintareqe <mizbauddintareq@gmail.com>
2022-09-10 23:01:35 +0600

    solved problem one

---
+++ practice-problems.js
@@ -0,0 +1,12 @@
+// Problem No:- 1
+var fruits = ["Apple", "Banana", "Orange"];
+
+var findBanana = fruits.indexOf("Banana");
+console.log(findBanana);
+
+fruits[1] = "Mango";
+console.log(fruits);
+
+fruits.pop();
+fruits.push("Watermelon");
+console.log(fruits);
